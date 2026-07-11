a6bd72024a7927317357c78d2d2487c64c5399c2
KamalAsFreelancer <kamal.pahan098@gmail.com>
2026-07-11 18:33:44 +0545

    variable project

---
+++ variableproj.js
@@ -0,0 +1,5 @@
+var name = "kamal";
+var age = 25;
+const language = "javascript";
+
+console.log("My name is " + name+ "and I am " +age+ "years old. I am learning "+ language+ "right now");
--- variables.js
+++ variables.js
@@ -1,12 +1,23 @@
+// var examples
 var age = 25;
 var name = "kamal";
 console.log("My name is " + name + " and  am " + age + " years old.");
 
+// let examples
 let city = "New York";
 console.log("I live in " + city);
 
 city = "Los Angeles";
 console.log("Now I live in " + city);
 
+// const examples
 const country = "USA";
 console.log("I live in " + country);
+
+// block scope example
+
+if (true) {
+    let x = 10;
+    const y = 20;
+    console.log("this is inside the block scope: x = " + x + ", y = " + y);
+}
