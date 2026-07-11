cda433c08633d6a1319ac19505c56d5f1d15fc0f
KamalAsFreelancer <kamal.pahan098@gmail.com>
2026-07-11 18:05:00 +0545

    Initial Push

---
+++ variables.js
@@ -0,0 +1,12 @@
+var age = 25;
+var name = "kamal";
+console.log("My name is " + name + " and  am " + age + " years old.");
+
+let city = "New York";
+console.log("I live in " + city);
+
+city = "Los Angeles";
+console.log("Now I live in " + city);
+
+const country = "USA";
+console.log("I live in " + country);
