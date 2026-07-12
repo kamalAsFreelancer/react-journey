ffb17c7d13d0cdaa585aeeeeee92a63286d89153
KamalAsFreelancer <kamal.pahan098@gmail.com>
2026-07-12 14:40:27 +0545

    loop practice

---
+++ loops.js
@@ -0,0 +1,27 @@
+// there are different types of loops 
+
+// 1. for loops
+// 2. while loop
+// 3. do...while loop
+// 4. for...of 
+// 5. for...in
+// 6.break
+// 7.continue
+// 8.nestedloop
+
+// for lop 
+// this is the most common loop in javascript.
+for (let i=1; i<=5; i++){
+    console.log(i);
+}
+
+// sum of the number using for loop...
+let sum = 0;
+
+for (let i = 1; i<=5;i++){
+    sum+=i;
+}
+console.log(sum);
+
+// While loop 
+// this loop runsd while the condition is true...
