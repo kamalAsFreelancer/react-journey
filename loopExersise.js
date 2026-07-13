d70166e69c0889dedc4a21029c66cb6ba7bb8b6e
KamalAsFreelancer <kamal.pahan098@gmail.com>
2026-07-13 19:01:07 +0545

    loop exersise

---
+++ loopExersise.js
@@ -0,0 +1,21 @@
+// Print the number 1-20 using for loop
+
+for (let i = 1; i<=20; i++)
+{
+    console.log(i);
+}
+
+// print only even number from 1-50
+for (let j=1; j<=50; j++)
+{
+    if (j % 2 === 0)
+        console.log(j);
+}
+
+// calculate the sum  of the num from 1-100
+let sum = 0;
+for (let k = 1; k<=100; k++){
+    sum += k;
+}
+
+console.log(sum);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,30 +1,4 @@
- // do while loop...
-// // the loops runs at least once, even the condition is false...
-
-// let j = 10;
-
-// do{
-//     console.log(j);
-//     j++;
-// } while(j <= 5);
-
-// //for of loop
-// // this is the most useful loop for the arrays... 
-
-// const fruits = ["apple", "banana", "orange"];
-
-// for (const fruit of fruits){
-//     console.log(fruit);
-// }
-
-// // foor in loop
-// // this is usde for objects...
-// const student = {
-//     name: "kamal",
-//     address: "Dhulabari",
-//     age: 24,
-// };
-
-// for (const key in student) {
-//     console.log(key, student[key]);
-// }
+for (let i = 1; i<=20; i++)
+{
+    console.log(i);
+}
